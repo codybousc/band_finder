e76153128a7f898c30dc102cd21cb3a6e230d466
Cody Bouscaren <codybousc@gmail.com>
2015-05-15 12:28:15 -0700

    Adds name validation

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -7,4 +7,9 @@ describe(Band) do
     new_venue.bands.push(new_band)
     expect(new_venue.bands).to(eq([new_band]))
   end
+
+  it("validates presence of name") do
+    new_band = Band.new({:name => ""})
+    expect(new_band.save()).to(eq(false))
+  end
 end
