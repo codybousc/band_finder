b35ccd7db1ab93166e946c963f166b1e032eacef
Cody Bouscaren <codybousc@gmail.com>
2015-05-15 11:03:03 -0700

    Adds new venue feature

--- app.rb
+++ app.rb
@@ -6,3 +6,26 @@ require('./lib/band')
 require('./lib/venue')
 require('pg')
 require('pry')
+
+
+get ('/') do
+  @all_venues = Venue.all()
+  erb(:index)
+end
+
+get ('/venues/new') do
+  erb(:venue_form)
+end
+
+post ('/venues') do
+  name = params.fetch("name")
+  new_venue = Venue.create({:name => name})
+  @all_venues = Venue.all()
+  erb(:index)
+end
+
+get ('/venues/:id') do
+  @venue = Venue.find(params.fetch("id").to_i())
+  @bands = @venue.bands
+  erb(:venue)
+end
